d8110b3720e6d94d5fdd5ff4ce13bb65283657be
Ian Sawyer <sawyerij@rose-hulman.edu>
2023-10-09 19:18:27 -0400

    buttons printing messages

--- FirebaseAuth/public/scripts/main.js
+++ FirebaseAuth/public/scripts/main.js
@@ -1,21 +1,23 @@
 var rhit = rhit || {};
 
-
-rhit.ListPageController = class {
-	constructor() {
-
-	}
-	
-	functionName() {
-		
-	}
-
-}
-
-
-
 rhit.main = function () {
 	console.log("Ready!");
+	
+	const inputEmailEl = document.querySelector("#inputEmail");
+	const inputPasswordEl = document.querySelector("#inputPassword");
+
+	document.querySelector("#signOutButton").onclick = (event) => {
+		console.log(`Sign out`);
+	}
+
+	document.querySelector("#createAccountButton").onclick = (event) => {
+		console.log(`Create Account for email: ${inputEmailEl.value} password: ${inputPasswordEl.value}`);
+	}
+
+	document.querySelector("#loginButton").onclick = (event) => {
+		console.log(`Login for email: ${inputEmailEl.value} password: ${inputPasswordEl.value}`);
+	}
+
 };
 
 rhit.main();
