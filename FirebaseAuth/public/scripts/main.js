afba132d3815d94a5e68a3c255850bcb85657bfd
Ian Sawyer <sawyerij@rose-hulman.edu>
2023-10-09 18:56:28 -0400

    cleanup

---
+++ FirebaseAuth/public/scripts/main.js
@@ -0,0 +1,21 @@
+var rhit = rhit || {};
+
+
+rhit.ListPageController = class {
+	constructor() {
+
+	}
+	
+	functionName() {
+		
+	}
+
+}
+
+
+
+rhit.main = function () {
+	console.log("Ready!");
+};
+
+rhit.main();
---
+++ template-master/public/scripts/main.js
@@ -0,0 +1,22 @@
+
+var rhit = rhit || {};
+
+
+rhit.ListPageController = class {
+	constructor() {
+
+	}
+	
+	functionName() {
+		
+	}
+
+}
+
+
+
+rhit.main = function () {
+	console.log("Ready!");
+};
+
+rhit.main();
