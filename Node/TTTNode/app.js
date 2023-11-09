51294b63d31846afb7d8073bc9936179323afb41
Ian Sawyer <sawyerij@rose-hulman.edu>
2023-11-08 23:19:47 -0500

    backend api complete

---
+++ Node/TTTNode/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+var app = express();
+app.use(express.static("public"));
+
+app.get("/api/getmove/:board", (req, res) => {
+    const boardString = req.params.board;
+
+    const openings = getOpenLocations(boardString);
+
+    const moveSelected = openings[Math.floor(Math.random() * openings.length)];
+    res.send({"move": moveSelected});
+});
+
+function getOpenLocations(boardString) {
+    const openLocations = [];
+    for (var i = 0; i < boardString.length; i++) {
+        if (boardString.charAt(i) == '-') {
+        openLocations.push(i)
+        }
+    }
+    return openLocations;
+}
+   
+
+
+
+
+app.listen(3000);
+
